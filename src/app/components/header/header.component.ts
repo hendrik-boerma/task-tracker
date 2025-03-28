3b11e58457b4e04905f774f616ee8aebe0d8c683
Hendrik Boerma <hendrikboerma@gmail.com>
2025-03-28 16:10:40 +0100

    state on false

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
-  showAddTask: boolean = true;
+  showAddTask: boolean = false;
   subscription: Subscription | null = null;
 
   constructor(private uiService:UiService, private router:Router) {
